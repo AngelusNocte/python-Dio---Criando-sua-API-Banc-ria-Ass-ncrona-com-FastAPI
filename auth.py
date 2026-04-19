f669cc51a0e3751e9e853ff6fbecb621002a2823
AngelusNocte <104323885+AngelusNocte@users.noreply.github.com>
2026-04-19 18:58:50 +0100

    Implement authentication functions in auth.py

---
+++ auth.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+
+SECRET_KEY = "secret"
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password, hashed):
+    return pwd_context.verify(password, hashed)
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    to_encode.update({"exp": datetime.utcnow() + timedelta(minutes=30)})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
